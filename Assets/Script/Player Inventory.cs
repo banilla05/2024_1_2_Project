743c1c545696fab15476aa5315a52b80aa67f16a
banilla05 <yeon.lavin05@gmail.com>
2024-11-13 12:05:49 +0900

    [Feat] Inven 아이템 소모 & 건설 & FloatingTextManager 제작

--- Assets/Script/Player Inventory.cs
+++ Assets/Script/Player Inventory.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,78 @@ public class PlayerInventory : MonoBehaviour
     //각각의 아이템 개수를 저장하는 변수
     public int crystalCount = 0;            //크리스탈 개수
     public int plantCount = 0;              //식물 개수
-    public int bushCount = 0;               //나무 개수
+    public int bushCount = 0;               //수풀 개수
     public int treeCount = 0;               //나무 개수
+
+    public void AddItem(ItemType itemtype, int amount)
+    {
+        //amount 만큼 여러번 AddItem 호출 
+        for (int i = 0; i < amount; i++)
+        {
+            AddItem(itemtype);
+        }
+    }
+
+    public bool Removeitem(ItemType itemType, int amount = 1)
+    {
+        //아이템 종류에 따른 다른 동작 수행
+        switch (itemType)
+        {
+            case ItemType.Crystal:
+                if (crystalCount >= amount)      //가지고 있는 개수가 충분한지 확인
+                {
+                    crystalCount -= amount; //크리스탈 개수 감소
+                    Debug.Log($"크리스탈 {amount} 사용! 현재 개수 :{crystalCount}");      //현재 크리스탈 개수 출력
+                    return true;
+                }
+                break;
+            case ItemType.Plant:
+                if (crystalCount >= amount)      //가지고 있는 개수가 충분한지 확인
+                {
+                    crystalCount -= amount; //식물 개수 감소
+                    Debug.Log($"식물 {amount} 사용! 현재 개수 :{crystalCount}");      //현재 크리스탈 개수 출력
+                    return true;
+                }
+                break;
+            case ItemType.Bush:
+                if (crystalCount >= amount)      //가지고 있는 개수가 충분한지 확인
+                {
+                    crystalCount -= amount; //크리스탈 개수 감소
+                    Debug.Log($"수풀 {amount} 사용! 현재 개수 :{crystalCount}");      //현재 크리스탈 개수 출력
+                    return true;
+                }
+                break;
+            case ItemType.Tree:
+                if (crystalCount >= amount)      //가지고 있는 개수가 충분한지 확인
+                {
+                    crystalCount -= amount; //크리스탈 개수 감소
+                    Debug.Log($"나무 {amount} 사용! 현재 개수 :{crystalCount}");      //현재 크리스탈 개수 출력
+                    return true;
+                }
+                break;
+        }
+
+        Debug.Log($"{itemType} 아이템이 부족합니다.");
+        return false;
+    }
+
+    //특정 아이템의 현재 개수를 반환하는 함수
+    public int GetItemCount(ItemType itemType)
+    {
+        switch(itemType)
+        {
+            case ItemType.Crystal:
+                return crystalCount;
+            case ItemType.Plant:
+                return plantCount;
+            case ItemType.Bush:
+                return bushCount;
+            case ItemType.Tree:
+                return treeCount;
+            default:
+                return 0;
+        }
+    }
 
     //아이템을 추가하는 함수, 아이템 종류에 따라 해당 아이템의 개수를 증가시킴
     public void AddItem(ItemType itemtype)
